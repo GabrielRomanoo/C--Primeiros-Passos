5b48affb577e1e3c9f93b5e23692eed340726221
gabriel <gabriel.romano.3194@hotmail.com>
2021-04-07 09:59:10 -0300

    variaveis e if

--- Program.cs
+++ Program.cs
@@ -12,6 +12,26 @@ namespace _1_PrimeiroProjeto
         {
             Console.WriteLine("Olá mundo!");
 
+            int idade = 20;
+            idade += 5;
+            Console.WriteLine(idade);
+
+            double numero = 2.0;
+            Console.WriteLine(numero);
+
+            if (numero > 1.0)
+            {
+                Console.WriteLine("oi");
+            }
+            else if (numero > 0.5)
+            {
+                Console.WriteLine("oi2");
+            }
+            else
+            {
+                Console.WriteLine("oi3");
+            }
+
             Console.WriteLine("Precione enter para continuar. . .");
             Console.ReadLine();
         }
